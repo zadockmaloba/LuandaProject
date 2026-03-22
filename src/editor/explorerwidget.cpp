9fb01e9c862e856eb5e7f1df63d77a5fc0251c8e
Zadock Maloba <55388270+zadockmaloba@users.noreply.github.com>
2026-03-22 08:11:08 +0300

    Add file tree model

--- src/editor/explorerwidget.cpp
+++ src/editor/explorerwidget.cpp
@@ -3,17 +3,20 @@
 #include <QTreeWidget>
 #include <QTreeView>
 #include <QHBoxLayout>
+#include <QFileSystemModel>
 #include <memory>
 
 struct ExplorerWidget::ExplorerWidgetPrivate {
     std::unique_ptr<QHBoxLayout> main_layout;
     std::unique_ptr<QTabWidget> tab_widget;
     std::unique_ptr<QTreeView>  file_tree_view;
+    std::unique_ptr<QFileSystemModel> file_tree_model;
 
     ExplorerWidgetPrivate() {
         this->main_layout = std::make_unique<QHBoxLayout>();
         this->tab_widget = std::make_unique<QTabWidget>();
         this->file_tree_view = std::make_unique<QTreeView>();
+        this->file_tree_model = std::make_unique<QFileSystemModel>();
     }
 };
 
@@ -22,8 +25,15 @@ ExplorerWidget::ExplorerWidget(QWidget *parent)
       _p{new ExplorerWidgetPrivate} {
 
     auto main_layout = _p->main_layout.get();
+    auto tab_widget = _p->tab_widget.get();
+    auto file_tree_view = _p->file_tree_view.get();
+    auto file_tree_model = _p->file_tree_model.get();
+
+    file_tree_view->setModel(file_tree_model);
+
     this->setLayout(main_layout);
-    main_layout->addWidget(_p->tab_widget.get(), 1);
+    main_layout->addWidget(tab_widget, 1);
+    tab_widget->addTab(file_tree_view, "Files");
 }
 
 ExplorerWidget::~ExplorerWidget() {
