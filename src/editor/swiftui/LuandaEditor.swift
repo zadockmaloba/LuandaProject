056f3890b5bfb164375de909e36a3df6f979c8cf
Zadock Maloba <55388270+zadockmaloba@users.noreply.github.com>
2026-03-23 08:15:56 +0300

    Add MetalView implementation and update LuandaEditor to include SettingsView

--- src/editor/swiftui/LuandaEditor.swift
+++ src/editor/swiftui/LuandaEditor.swift
@@ -1,9 +1,25 @@
 import SwiftUI
 
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+            .frame(minWidth: 400, minHeight: 300)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Luanda Editor")
-            .frame(minWidth: 800, minHeight: 600)
+        NavigationSplitView {
+            List {
+                Text("Item 1")
+                Text("Item 2")
+                Text("Item 3")
+            }
+            .listStyle(SidebarListStyle())
+        } detail: {
+            MetalView(color: .blue)
+                .edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
@@ -13,6 +29,14 @@ struct LuandaEditor: App {
         WindowGroup {
             ContentView()
         }
+        .commands {
+            SidebarCommands()
+        }
+#if os(macOS)
+        Settings {
+            SettingsView()
+        }
+#endif
     }
 }
 
---
+++ src/editor/swiftui/MetalView.swift
@@ -0,0 +1,83 @@
+// REF: https://medium.com/@giikwebdeveloper/metal-view-for-swiftui-93f5f78ec36a
+import MetalKit
+import SwiftUI
+
+// The coordinator class implements the MTKViewDelegate protocol and an instance of
+// it is used as the delegate for the wrapped MTKView.
+class MetalViewCoordinator: NSObject, MTKViewDelegate {
+  let device: MTLDevice
+  // The Metal Programming Guide recommends that non-transient objects
+  // including command queues are reused, especially in performance-sensitive
+  // code.
+  let commandQueue: MTLCommandQueue
+
+  override init() {
+    guard let device = MTLCreateSystemDefaultDevice(),
+      let queue = device.makeCommandQueue()
+    else {
+      fatalError("could not create device or command queue")
+    }
+    // Technically, we don't need to save the device in the coordinator. The
+    // MTKView must have its device property set so this is an easy way to
+    // ensure that the devices used by the coordinator and the view are
+    // identical.
+    self.device = device
+    self.commandQueue = queue
+    super.init()
+  }
+
+  func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
+
+  // Most importantly, the delegate implements the draw(in:) method which we don't call
+  // directly. Rather, we request for it to be called by calling the draw() method of
+  // the wrapped MTKView.
+  func draw(in view: MTKView) {
+    guard let buffer = self.commandQueue.makeCommandBuffer(),
+      let descriptor = view.currentRenderPassDescriptor,
+      let encoder = buffer.makeRenderCommandEncoder(descriptor: descriptor)
+    else {
+      fatalError("could not create buffer, render pass descriptor, or encoder")
+    }
+    encoder.endEncoding()
+    if let drawable = view.currentDrawable {
+      buffer.present(drawable)
+      buffer.commit()
+    }
+  }
+}
+
+struct MetalView: NSViewRepresentable {
+  @Environment(\.self) var environment  // needed to resolve Color instances
+  var color: Color                      // allows us to inject a clear color
+
+  func makeCoordinator() -> MetalViewCoordinator {
+    MetalViewCoordinator()
+  }
+
+  // Creating the wrapped MTKView
+  func makeNSView(context: Context) -> MTKView {
+    let view = MTKView()
+    view.delegate = context.coordinator
+    view.device = context.coordinator.device
+    print("made the MTKView")
+    return view
+  }
+
+  // Called when the view needs to be updated, incl. because the value of the
+  // Binding changed. It gives us a chance to call draw() which in turn will
+  // call draw(in:) on the delegate.
+  func updateNSView(_ view: MTKView, context: Context) {
+    let resolved = self.color.resolve(in: self.environment)
+    view.clearColor = MTLClearColor(
+      red: Double(resolved.red),
+      green: Double(resolved.green),
+      blue: Double(resolved.blue),
+      alpha: Double(resolved.opacity)
+    )
+    view.draw()
+  }
+}
+
+#Preview {
+  MetalView(color: Color.red)
+}
